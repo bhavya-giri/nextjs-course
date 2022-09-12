0367ca72f16390d3cca8bb7338bffaba0b28bfd0
Bhavya Giri <bhavya.giri44@gmail.com>
2022-09-12 14:39:29 +0530

    dynamic extract

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { useRouter } from 'next/router'
 const DetailsPage = () => {
+  const router = useRouter()
+  console.log(router.query.newsId)
   return (
     <div>DetailsPage</div>
   )
